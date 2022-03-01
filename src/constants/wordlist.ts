ec5b113f634274fccce8e63cc41f88be5c686498
Ryan Sherota <ryansherota@Ryans-MacBook-Pro.local>
2022-03-01 12:02:46 +0200

    ‘changes’

--- src/constants/wordlist.ts
+++ src/constants/wordlist.ts
@@ -1,41 +1,41 @@
 export const WORDS = [
-  'ryans',
-  'bussy', 
-  'swift',
-  'vibes', // feb 1
-  'pinky', // feb 2
-  'elonu', // feb 3
-  'sarah', // feb 4
-  'boufy', // feb 5 ? day 7
-  'ditzy', // feb 6
-  'betta', // feb 7
-  'katey', // feb 8
-  'colin', // feb 9
-  'julia', // feb 10
-  'kitty', // feb 11
-  'perry', // feb 12
-  'abbaw', // feb 13
-  'capet', // feb 14
-  'meoww', // feb 15
-  'seven', // feb 16
-  'kylie', // feb 17
-  'baefy', // feb 18
-  'piano', // feb 19
-  'dania', // feb 20
-  'point',
-  'brade',
-  'omggg',
-  'handm',
-  'mamis',
-  'slays',
-  'loveu',
-  'point',
-  'brade',
-  'omggg',
-  'handm',
-  'mamis',
-  'slays',
-  'loveu',
+  // 'ryans',
+  // 'bussy', 
+  // 'swift',
+  // 'vibes', // feb 1
+  // 'pinky', // feb 2
+  // 'elonu', // feb 3
+  // 'sarah', // feb 4
+  // 'boufy', // feb 5 ? day 7
+  // 'ditzy', // feb 6
+  // 'betta', // feb 7
+  // 'katey', // feb 8
+  // 'colin', // feb 9
+  // 'julia', // feb 10
+  // 'kitty', // feb 11
+  // 'perry', // feb 12
+  // 'abbaw', // feb 13
+  // 'capet', // feb 14
+  // 'meoww', // feb 15
+  // 'seven', // feb 16
+  // 'kylie', // feb 17
+  // 'baefy', // feb 18
+  // 'piano', // feb 19
+  // 'dania', // feb 20
+  // 'point',
+  // 'brade',
+  // 'omggg',
+  // 'handm',
+  // 'mamis',
+  // 'slays',
+  // 'loveu',
+  // 'point',
+  // 'brade',
+  // 'omggg',
+  // 'handm',
+  // 'mamis',
+  // 'slays',
+  // 'loveu',
   'bitch',
   'boner',
   'chutt',
@@ -44,5 +44,5 @@ export const WORDS = [
   'dbuss',
   'disco',
   'beads',
-  'socks',
+  'socks', // march 8
 ]
